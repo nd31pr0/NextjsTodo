bda9cf6bcb34f3ca8ca3fd4405d2de8628bdd5c1
Ndzi Nfor <ndzinfor@gmail.com>
2024-06-22 09:22:31 +0100

    Create dummy todo table

--- todo-list/app/components/AddTask.tsx
+++ todo-list/app/components/AddTask.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { AiOutlinePlus } from "react-icons/ai";
+
 
 function AddTask() {
   return (
-    <div>
-        <button className='btn btn-primary w-full'>Add new task</button>
+    <div className='flex gap-4'>
+        <button className='btn btn-primary w-full'>Add new task
+        <AiOutlinePlus  size={18} className="ml-2"/></button>
+        
     </div>
   )
 }
--- todo-list/app/page.tsx
+++ todo-list/app/page.tsx
@@ -1,10 +1,12 @@
 import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold">Todo List App</h1>
         <AddTask />
+        <TodoList />
       </div>
     </main>
   );
