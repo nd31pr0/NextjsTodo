87fbc3a584c4ac5bd066c3f71bffa3de667fc79f
Ndzi Nfor <ndzinfor@gmail.com>
2024-06-24 17:26:10 +0100

    create api for edit and delete

--- todo-list/api.ts
+++ todo-list/api.ts
@@ -18,3 +18,21 @@ export const addTodo = async (todo: ITask): Promise<ITask> => {
     const newTodo = await res.json();
     return newTodo;
 }
+
+export const editTodo = async (todo: ITask): Promise<ITask> => {
+    const res = await fetch(`${baseUrl}/${todo.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    const updatedTodo = await res.json();
+    return updatedTodo;
+}
+
+export const deleteTodo = async (id: string): Promise<void> => {
+    await fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE',
+    })
+}
